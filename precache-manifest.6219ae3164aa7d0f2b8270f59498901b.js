a340375ad70c76ae02bec2ff401c63b776c9e681
snaketh4x0r <snaketh4xor@protonmail.com>
2020-03-02 13:30:17 +0530

    Updates

---
+++ precache-manifest.6219ae3164aa7d0f2b8270f59498901b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1f34b73a8cf76f209407ca58f02fa0",
+    "url": "/social-stream_frontend/index.html"
+  },
+  {
+    "revision": "16a56af30818ac165c82",
+    "url": "/social-stream_frontend/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "272423d387ebb1a97e9b",
+    "url": "/social-stream_frontend/static/css/main.1c668ba5.chunk.css"
+  },
+  {
+    "revision": "16a56af30818ac165c82",
+    "url": "/social-stream_frontend/static/js/2.3c7ec6b7.chunk.js"
+  },
+  {
+    "revision": "eefa402c39a7d49bf58197a0a7cd2ee5",
+    "url": "/social-stream_frontend/static/js/2.3c7ec6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272423d387ebb1a97e9b",
+    "url": "/social-stream_frontend/static/js/main.9461cb92.chunk.js"
+  },
+  {
+    "revision": "3eeb039cb3a8ed5d206f",
+    "url": "/social-stream_frontend/static/js/runtime-main.ed7f50dd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-stream/precache-manifest.3e97c156cafa26aef673f360c8941daf.js"
+  "/social-stream_frontend/precache-manifest.6219ae3164aa7d0f2b8270f59498901b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-stream/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-stream_frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
